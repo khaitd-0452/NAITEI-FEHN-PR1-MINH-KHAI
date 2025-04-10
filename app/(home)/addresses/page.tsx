b586a5e57d246b300968dbbc0b64ad91ea102dc1
khaitd-3535 <tran.duc.khai@sun-asterisk.com>
2025-04-10 10:14:46 +0700

    [task][86455] create address page ui

---
+++ app/(home)/addresses/page.tsx
@@ -0,0 +1,25 @@
+﻿import PageBreadCrumb from "@/components/layout/page_bread_crumb";
+import React from "react";
+import Image from "next/image";
+import AddressesMain from "@/components/addresses/addresses-main";
+
+export default function AddressPage() {
+  return (
+    <main className="container mx-auto max-w-[1200px] mt-6 mb-40 space-y-8 px-4 ">
+      <PageBreadCrumb />
+      <div className="flex flex-col w-full justify-start">
+        <h2 className="text-xl md:text-2xl font-medium text-gray-700">
+          ĐỊA CHỈ
+        </h2>
+        <Image
+          src={"/images/titleleft-dark.png"}
+          alt="title underline"
+          width={200}
+          height={100}
+          className="w-[70px] mt-2 mb-4"
+        />
+        <AddressesMain />
+      </div>
+    </main>
+  );
+}
---
+++ components/addresses/address-card.tsx
@@ -0,0 +1,46 @@
+﻿import { Address } from "@/lib/types/address";
+
+export default function AddressCard({ address }: { address: Address }) {
+  const fieldMapping = [
+    { label: "Tên", value: address.first_name },
+    { label: "Họ & tên đệm", value: address.last_name },
+    { label: "Công ty", value: address.company },
+    { label: "Địa chỉ", value: address.address },
+    { label: "Thành phố", value: address.city },
+    { label: "Quốc Tịch", value: address.country },
+    { label: "Postal/Zip Code", value: address.zipcode },
+    { label: "Phone", value: address.phone },
+  ];
+
+  return (
+    <div className="bg-white border border-gray-200 rounded-lg shadow-sm mb-6 overflow-hidden">
+      <div className="px-6 py-4">
+        {address.default && (
+          <div className="mb-3">
+            <span className="inline-block bg-blue-100 text-blue-800 text-xs px-2 py-1 rounded">
+              Địa chỉ mặc định
+            </span>
+          </div>
+        )}
+
+        <div className="">
+          {fieldMapping.map((field, index) => (
+            <div key={index} className="flex py-4 border-b border-gray-100">
+              <div className="w-1/3 text-gray-500">{field.label}</div>
+              <div className="w-2/3 text-gray-600">{field.value}</div>
+            </div>
+          ))}
+        </div>
+
+        <div className="mt-4 text-right">
+          <a
+            href={`/addresses/edit/${address.id}`}
+            className="text-blue-500 hover:text-blue-700"
+          >
+            Chỉnh sửa địa chỉ
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
